72be27341c119b2204d9be2f0666e35267fcf26c
John-Robert Scholz <john.robert.scholz@gmail.com>
2020-09-29 10:47:08 +0200

    doc

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -90,25 +90,17 @@ html_static_path = ['_static']
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
+    'logo_only'             : False,
+    'canonical_url'         : 'https://seisglitch.readthedocs.io/en/latest/index.html',
+    'display_version'       : True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links'      :  False,
+    'vcs_pageview_mode'         :  '',
+    #'style_nav_header_background': 'white',    
+    # Toc options
     'collapse_navigation': False,
     'sticky_navigation':   True,
     'navigation_depth':    4,
     'includehidden':       True,
     'titles_only':         False
 }
-
-#html_theme_options = {
-#    'logo_only'             : False,
-#    'canonical_url'         : 'https://seisglitch.readthedocs.io/en/latest/index.html',
-#    'display_version'       : True,
-#    'prev_next_buttons_location': 'bottom',
-#    'style_external_links'      :  False,
-#    'vcs_pageview_mode'         :  '',
-#    'style_nav_header_background': 'white',    
-#    # Toc options
-#    'collapse_navigation': False,
-#    'sticky_navigation':   True,
-#    'navigation_depth':    4,
-#    'includehidden':       True,
-#    'titles_only':         False
-#}
