6f24877a760eec3d31ce09cf4b4dfe3c3411489e
John-Robert Scholz <john.robert.scholz@gmail.com>
2020-09-29 10:32:14 +0200

    doc

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -83,7 +83,7 @@ html_theme = 'sphinx_rtd_theme'  # alabaster, pyramid, haiku, scrolls, classic, 
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo        = "glitch.png"
+#html_logo        = "glitch.png"
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
