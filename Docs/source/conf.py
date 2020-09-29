85ddde8036a417381df6b25b2d776372f83d7743
John-Robert Scholz <john.robert.scholz@gmail.com>
2020-09-29 10:12:27 +0200

    change docs

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -80,7 +80,11 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'  # alabaster, pyramid, haiku, scrolls, classic, nature, sphinx_rtd_theme
-html_logo  = 'glitch.png',
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_logo        = "glitch.png"
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -103,7 +107,3 @@ html_theme_options = {
 }
 
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
